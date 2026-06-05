f08e1e743b028872462380fa7661a78e5daf1d08
dharshini-n13 <dharshini13816@gmail.com>
2026-06-05 18:47:28 +0530

    fix: allow all CORS origins

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
